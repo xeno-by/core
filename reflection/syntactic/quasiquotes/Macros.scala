e326b2587f04e5cfaa947df45f1440e36770f16b
Eugene Burmako <xeno.by@gmail.com>
2014-06-13 01:10:40 +0200

    support for no-dot unquoting

--- reflection/syntactic/quasiquotes/Macros.scala
+++ reflection/syntactic/quasiquotes/Macros.scala
@@ -2,17 +2,20 @@ package scala.reflect
 package syntactic.quasiquotes
 
 import scala.reflect.macros.whitebox.Context
-import scala.reflect.core.{Tree => PalladiumTree}
 import org.scalareflect.adt._
+import scala.reflect.syntactic.parsers.ParseSyntaxError
 
 class Macros[C <: Context](val c: C) extends AdtReflection with NewLiftables {
   val u: c.universe.type = c.universe
   import c.universe.{Tree => _, _}
   import c.universe.{Tree => ScalaTree}
+  import scala.reflect.core.{Tree => PalladiumTree}
   def apply(macroApplication: ScalaTree, parse: String => PalladiumTree): ScalaTree = {
-    val q"$_($_.apply(..$parts)).$_.apply[..$_](..$args)" = macroApplication
-    if (args.length != 0) c.abort(macroApplication.pos, "unquoting is not supported yet")
-    val liveTree: PalladiumTree = parse(parts.map{case q"${part: String}" => part}.mkString(""))
+    val q"$_($_.apply(..$partlits)).$_.apply[..$_](..$argtrees)" = macroApplication
+    val parts = partlits.map{ case q"${part: String}" => part }
+    val args = argtrees.map(tree => c.freshName(tree.toString) -> tree)
+    val snippet = (parts.init.zip(args).flatMap{ case (part, (id, _)) => List(part, s"`$id`") } :+ parts.last).mkString("")
+    val liveTree: PalladiumTree = parse(snippet)
     val reifiedTree: ScalaTree = implicitly[Liftable[PalladiumTree]].apply(liveTree)
     reifiedTree
   }
